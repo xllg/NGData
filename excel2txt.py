56e3d3bab7b6264ccda3931908a5ac546b9dbbbb
xllg <512412982@qq.com>
2020-08-10 11:05:14 +0800

    make train_data

--- excel2txt.py
+++ excel2txt.py
@@ -15,28 +15,29 @@ from operator import itemgetter
 def loadDataFromExcel(filesrc, usesheet, useclos):
     df = pd.read_excel(filesrc, sheet_name=usesheet,usecols=useclos)
     ds = df[df[df.columns[0]].notnull()] # 选出大类不为空的数据
-    da = ds[ds[ds.columns[0]].str.contains('环境卫生')] #选出大类数据
-    db = da[da[da.columns[1]].notnull()] # 选出小类不为空的数据
-    db = db[db[db.columns[1]].str.contains('环卫作业')] #选出小类数据
+    # da = ds[ds[ds.columns[0]].str.contains('环境卫生')] #选出大类数据
+    db = ds[ds[ds.columns[1]].notnull()] # 选出小类不为空的数据
+    # db = db[db[db.columns[1]].str.contains('环卫作业')] #选出小类数据
     dc = db[db[db.columns[2]].notnull()] # 选出细类不为空的数据
-    dc = dc[dc[dc.columns[2]].str.contains('垃圾收运')] #选出细类数据
-    data = dc[dc.columns[3]].values #使用来电内容进行分词词频统计
-    text = ''.join(data)
-    return data, text
+    # dc = dc[dc[dc.columns[2]].str.contains('垃圾收运')] #选出细类数据
+    # data = dc[dc.columns[3]].values  # 使用来电内容进行分词词频统计
+    data = dc.values #使用来电内容进行分词词频统计
+    # text = ''.join(data)
+    # return data, text
+    return data  #返回 [[大类]，[小类],[细类],[来电内容]]
 # 结巴分词
 def jiebaCut(data):
     words = []
     strs = []
-    jieba.enable_paddle()  # 启动paddle模式。 0.40版之后开始支持，早期版本不支持
-    for i in tqdm(range(len(data))):
-        seg_list = jieba.lcut(data[i], use_paddle=True)
-        strs.append('/'.join(seg_list))
-        words += seg_list
-    print(str(len(strs)) + "条来话共计" + str(len(words)) + "个分词被记录！")
+    # for i in tqdm(range(len(data))):
+    seg_list = jieba.lcut(data, use_paddle=True)
+    strs.append(' '.join(seg_list))
+        # words += seg_list
+    # print(str(len(strs)) + "条来话共计" + str(len(words)) + "个分词被记录！")
     # 词频统计
-    word_counts = collections.Counter(words)
-    word_counts_sort = sorted(word_counts.items(), key = itemgetter(1),reverse=True)
-    return strs, word_counts_sort
+    # word_counts = collections.Counter(words)
+    # word_counts_sort = sorted(word_counts.items(), key = itemgetter(1),reverse=True)
+    return strs
 
 def jiebaTFIDF(text, topk):
     keywords = jieba.analyse.extract_tags(text, topK=topk, withWeight=True, allowPOS=())
@@ -51,12 +52,37 @@ def write2File(strs, word_counts_sort):
         for line in strs:
             fc.write(line + '\n')
     print("分词结果写入文件成功!")
-
+def makeData():
+    jieba.enable_paddle()  # 启动paddle模式。 0.40版之后开始支持，早期版本不支持
+    data = loadDataFromExcel('../data/总表6.23-1.xlsx', '区市县派', [6, 7, 8, 11])
+    big_class = ['环境保护', '市容秩序与广告招牌', '规划执法', '住房执法', '环境卫生', '市政设施', '景观照明与功能性照明', '其他']
+    give_class = {}
+    with open('../data/class.txt', 'r') as fc:
+        for line in fc.readlines():
+            give_class[line.split('\t')[0]] = line.split('\t')[1].strip('\n')
+    fina_class = []
+    for item in data:
+        if item[0] in big_class:
+            now_class = '-'.join(item[:3])
+            if now_class in give_class:
+                class_num = give_class[now_class]
+                seg_list = jiebaCut(item[3])
+                fina_class.append(''.join(seg_list) + '\t' + class_num)
+    with open('../data/cut_strs.txt', 'w') as fc:
+        for line in fina_class:
+            fc.write(line + '\n')
+    print("Done!")
+def wordCount():
+        text = []
+        count = 0
+        with open('../data/cut_strs.txt', 'r') as fc:
+            for line in tqdm(fc.readlines()):
+                text.append(line.split('\t')[0])
+        for item in text:
+            str = item.split(' ')
+            count += len(str)
+        count = count / len(text)
+        print(count)
 
 if __name__ == '__main__':
-    data, text = loadDataFromExcel('../data/6月总表.xlsx', '下级办理', [7, 8, 9, 11])
-    keywords = jiebaTFIDF(text, 100)
-    for key in keywords:
-        print(key)
-    # with open('../data/cut_strs.txt', 'w') as fc:
-    #     fc.write(text )
+    wordCount()
--- jieba_03_extract_tags.py
+++ jieba_03_extract_tags.py
@@ -12,3 +12,12 @@ text = '关键词是能够表达文档中心内容的词语，常用于计算机
 
 keywords = jieba.analyse.extract_tags(text, topK=20, withWeight=True, allowPOS=())
 print(keywords)
+
+import jieba.analyse
+
+text = '关键词是能够表达文档中心内容的词语，常用于计算机系统标引论文内容特征、' \
+       '信息检索、系统汇集以供读者检阅。关键词提取是文本挖掘领域的一个分支，是文本检索、' \
+       '文档比较、摘要生成、文档分类和聚类等文本挖掘研究的基础性工作'
+
+keywords = jieba.analyse.extract_tags(text, topK=20, withWeight=True, allowPOS=())
+print(keywords)
